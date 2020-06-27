bbb1f7d0426443a598415a041e3920ad042a7483
Michael Schwartz <mykesx@gmail.com>
2020-06-27 13:52:17 -0700

    list, bochsrc clock tweaks

--- kernel/Exec/x86/pci.cpp
+++ kernel/Exec/x86/pci.cpp
@@ -1,5 +1,7 @@
 #include <Exec/x86/pci.h>
 
+// https : //github.com/nickwanninger/chariot/blob/cc3649af22126d21e3ff77296f73ec73157b1b8c/kernel/dev/pci.cpp
+
 const TUint16 ADDRESS_PORT = 0xcf8;
 const TUint16 DATA_PORT = 0xcfc;
 
@@ -17,6 +19,9 @@ const TUint8 OFFSET_IRQ = 0x3c;           // lower 8 bits
 const TUint16 MULTIFUNCTION_DEVICE = 0x80;
 const TUint16 INVALID_VENDOR_ID = 0xffff;
 
+const TUint16 SUBCLASS_PCI_TO_PCI_BRIDGE = 0x04;
+const TUint16 CLASS_CODE_BRIDGE_DEVICE = 0x06;
+
 class PCI_IO {
 public:
   PCI_IO() {}
@@ -32,7 +37,7 @@ public:
   TUint32 ReadField(TUint32 aAddress, TUint8 aOffset) {
     outl(ADDRESS_PORT, aAddress | (aOffset & 0xfc));
     TUint32 ret = inl(DATA_PORT);
-    dlog("      ReadField(%x, %02x) = %08x\n", aAddress, aOffset, ret);
+    // dlog("      ReadField(%x, %02x) = %08x\n", aAddress, aOffset, ret);
     return ret;
   }
 
@@ -46,14 +51,14 @@ public:
   TUint16 GetDeviceId(TUint32 aAddress) {
     // upper 16 bits
     TUint16 device_id = (ReadField(aAddress, OFFSET_DEVICE_ID) >> 16) & 0xffff;
-    dlog("      device id: %x\n:, device_id\n");
+    // dlog("      device id: %x\n:, device_id\n");
     return device_id;
   }
 
   TUint16 GetVendorId(TUint32 aAddress) {
     // lower 16 bits
     TUint16 vendor_id = ReadField(aAddress, OFFSET_VENDOR_ID) & 0xffff;
-    dlog("      vendor id: %x\n", vendor_id);
+    // dlog("      vendor id: %x\n", vendor_id);
     return vendor_id;
   }
 
@@ -92,49 +97,105 @@ public:
   }
 };
 
-void PCI::ScanBus(TUint8 aBusNumber) {
-  TUint8 func = 0;
-  for (TUint8 device = 0; device < 32; device++) {
-    TUint32 addr = mIO->GetAddress(aBusNumber, device, func);
-    TUint16 type = mIO->GetHeaderType(addr);
-    dlog("    ScanBus(%d) addr(%x) type(%x)\n", aBusNumber, addr, type);
-    if (type & MULTIFUNCTION_DEVICE) {
-      dlog(" MULTIFUNCTION\n");
-      for (func = 0; func < 7; func++) {
-        // check function
-      }
-    }
-    else {
-      // check funciton
-    }
+static PCI_IO sio;
+
+static const char *class_name(TInt16 aClass) {
+  static const char *class_names[] = {
+    [0x00] = "Unclassified",
+    [0x01] = "Mass Storage Controller",
+    [0x02] = "Network Controller",
+    [0x03] = "Display Controller",
+    [0x04] = "Multimedia Controller",
+    [0x05] = "Memory Controller",
+    [0x06] = "Bridge Device",
+    [0x07] = "Simple Communication Controller",
+    [0x08] = "Base System Peripheral",
+    [0x09] = "Input Device Controller",
+    [0x0A] = "Docking Station",
+    [0x0B] = "Processor",
+    [0x0C] = "Serial Bus Controller",
+    [0x0D] = "Wireless Controller",
+    [0x0E] = "Intelligent Controller",
+    [0x0F] = "Satellite Communication Controller",
+    [0x10] = "Encryption Controller",
+    [0x11] = "Signal Processing Controller",
+    [0x12] = "Processing Accelerator",
+    [0x13] = "Non-Essential Instrumentation",
+    [0x14] = "(Reserved)",
+  };
+  return (aClass > 0x14) ? "Unknown" : class_names[aClass];
+}
+
+void PCI::CheckFunction(TUint8 aBus, TUint8 aDevice, TUint8 aFunction) {
+  TUint32 addr = sio.GetAddress(aBus, aDevice, aFunction);
+  TUint16 vendorId = sio.GetVendorId(addr),
+          deviceId = sio.GetDeviceId(addr),
+          baseClass = sio.GetClassCode(addr),
+          subClass = sio.GetSubclassCode(addr);
+
+  if (baseClass == 0xff) {
+    return;
+  }
+
+  PCIDevice *dev = new PCIDevice(class_name(baseClass));
+  dev->mBus = aBus;
+  dev->mDevice = aDevice;
+  dev->mFunction = aFunction;
+  dev->mVendorId = vendorId;
+  dev->mDeviceId = deviceId;
+  dev->mClassId = baseClass;
+  dev->mSubclassId = subClass;
+  mDeviceList.AddTail(*dev);
+
+  dlog("      vendorId(%x), baseClass %x(%s) subClass(%x) addr(%x) BRIDGE(%x) SUBBRIDGE(%x)\n",
+    vendorId, baseClass, class_name(baseClass), subClass, addr, CLASS_CODE_BRIDGE_DEVICE, SUBCLASS_PCI_TO_PCI_BRIDGE);
+
+  if ((baseClass == CLASS_CODE_BRIDGE_DEVICE) && (subClass == SUBCLASS_PCI_TO_PCI_BRIDGE)) {
+    ScanBus(sio.GetSecondary(addr));
   }
 }
 
-void PCI::Scan() {
-  TUint16 type = mIO->GetHeaderType(mIO->GetAddress(0, 0, 0));
-  dlog("  SCAN(%x)\n", type);
-  if ((type & MULTIFUNCTION_DEVICE)) {
-    for (TUint8 bus = 0; bus < 8; bus++) {
-      dlog("    bus(%d)\n", bus);
-      TUint16 vendorId = mIO->GetVendorId(mIO->GetAddress(0, 0, bus));
-      dlog("    bus(%d) vendorId(%x)\n", bus, vendorId);
-      if (mIO->GetVendorId(mIO->GetAddress(0, 0, bus)) == INVALID_VENDOR_ID) {
-        ScanBus(bus);
+void PCI::ScanBus(TUint8 aBusNumber) {
+  TUint8 func = 0;
+
+  for (TUint8 device = 0; device < 32; device++) {
+    TUint32 addr = sio.GetAddress(aBusNumber, device, func);
+    TUint16 type = sio.GetHeaderType(addr);
+    if (type != 0xff) {
+      dlog("    ScanBus(%d) addr(%x) type(%x)\n", aBusNumber, addr, type);
+      if ((type & MULTIFUNCTION_DEVICE) != 0) {
+        dlog("      MULTIFUNCION\n");
+        for (func = 0; func < 7; func++) {
+          CheckFunction(aBusNumber, device, func);
+        }
+      }
+      else {
+        CheckFunction(aBusNumber, device, 0);
       }
     }
-  }
-  else {
-    ScanBus(0);
   }
 }
 
 PCI::PCI() {
-  mIO = new PCI_IO();
   dlog("CONSTRUCT PCI\n");
-  Scan();
+  TUint16 type = sio.GetHeaderType(sio.GetAddress(0, 0, 0));
+  if ((type & MULTIFUNCTION_DEVICE) == 0) {
+    dlog("  Single PCI host controller\n");
+    ScanBus(0);
+  }
+  else {
+    dlog("  Multiple PCI host controllers\n");
+    for (TInt8 bus = 0; bus < 32; bus++) {
+      ScanBus(bus);
+    }
+  }
+  dlog("Device List:\n");
+  for (PCIDevice *d = (PCIDevice *)mDeviceList.First(); !mDeviceList.End(d); d = (PCIDevice *)d->mNext) {
+    d->Dump();
+  }
+  dprint("\n\n");
+  bochs;
 }
 
 PCI::~PCI() {
-  delete mIO;
 }
-
